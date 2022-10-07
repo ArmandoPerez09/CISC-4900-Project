b2c9603bbb5c5685a175ac9c77100d063485c130
thisisnotdevin <devin1299@outlook.com>
2022-10-06 22:55:55 -0400

    updated buttons

--- src/script.js
+++ src/script.js
@@ -1,5 +1,14 @@
 const readMoreBtn =  document.querySelector('.read-more-btn');
 const text = document.querySelector('.text');
+var map;
+var infowindow;
+var service;
+var service2;
+var service3;
+var service4;
+var newYork = {lat: 40.7549, lng: -73.9840};
+
+ // WORKING! allows the server to load anything inside the env file
 
 // function initMap() {
 //     var location = {lat: 40.712776, lng: -74.005974};
@@ -8,16 +17,451 @@ const text = document.querySelector('.text');
 //         center: location
 //     }); 
 // }
+
+function getMusic() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: newYork,
+        zoom: 14
+    });
+    var request = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['music'], //also work with types, look at APi suported types
+      };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    var request2 = {
+        location: newYork,
+        radius: '5500',
+        type: ['night_club'], 
+      };
+
+    service2 = new google.maps.places.PlacesService(map);
+    service2.nearbySearch(request2, callback);
+
+    var request3 = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['song'], //also work with types, look at APi suported types
+      };
+
+    service3 = new google.maps.places.PlacesService(map);
+    service3.nearbySearch(request3, callback);
+
+   function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(place);
+    }
+  }
+}
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map : map,
+            position : placeLoc
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        
+        });
+       
+    }
+   
+    infowindow = new google.maps.InfoWindow();
+}
+function getArt() { 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: newYork,
+        zoom: 14
+    });
+    var request = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['art'], //also work with types, look at APi suported types
+        
+      };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    var request2 = {
+        location: newYork,
+        radius: '5500',
+        type: ['art_gallery'],
+    };
+    service2 = new google.maps.places.PlacesService(map);
+    service2.nearbySearch(request2, callback);
+
+    var request3 = {
+        location: newYork,
+        radius: '5500',
+        type: ['museum'],
+    };
+    service3 = new google.maps.places.PlacesService(map);
+    service3.nearbySearch(request3, callback);
+
+   function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(place);
+    }
+  }
+}
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map : map,
+            position : placeLoc
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        
+        });
+       
+    }
+   
+    infowindow = new google.maps.InfoWindow();
+}
+function getOutdoor() { 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: newYork,
+        zoom: 14
+    });
+    var request = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['outdoor'], //also work with types, look at APi suported types
+        
+      };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    var request2 = {
+        location: newYork,
+        radius: '5500',
+        type: ['park'], //also work with types, look at APi suported types
+        
+      };
+
+    service2 = new google.maps.places.PlacesService(map);
+    service2.nearbySearch(request2, callback);
+
+    
+   function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(place);
+    }
+  }
+}
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map : map,
+            position : placeLoc
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        
+        });
+       
+    }
+   
+    infowindow = new google.maps.InfoWindow();
+}
+function getRestaurant() { 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: newYork,
+        zoom: 14
+    });
+    var request = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['restaurant'], //also work with types, look at APi suported types
+        
+      };
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+    
+    var request2 = {
+        location: newYork,
+        radius: '5500',
+        type: ['bakery'],
+        
+      };
+    service2 = new google.maps.places.PlacesService(map);
+    service2.nearbySearch(request2, callback);
+
+    var request3 = {
+        location: newYork,
+        radius: '5500',
+        type: ['bar'],
+        
+      };
+    service3 = new google.maps.places.PlacesService(map);
+    service3.nearbySearch(request3, callback);
+
+    var request4 = {
+        location: newYork,
+        radius: '5500',
+        type: ['cafe'],
+        
+      };
+    service4 = new google.maps.places.PlacesService(map);
+    service4.nearbySearch(request4, callback);
+
+    var request5 = {
+        location: newYork,
+        radius: '5500',
+        type: ['restaurant'],
+        
+      };
+    service5 = new google.maps.places.PlacesService(map);
+    service5.nearbySearch(request5, callback);
+
+    
+
+   function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(place);
+    }
+  }
+}
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map : map,
+            position : placeLoc
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        
+        });
+       
+    }
+   
+    infowindow = new google.maps.InfoWindow();
+}
+function getExercise() { 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: newYork,
+        zoom: 14
+    });
+    var request = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['exercise'], //also work with types, look at APi suported types
+        
+      };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    var request2 = {
+        location: newYork,
+        radius: '5500',
+        type: ['gym'],
+        
+      };
+
+    service2 = new google.maps.places.PlacesService(map);
+    service2.nearbySearch(request2, callback);
+
+    var request3 = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['workout'], 
+        
+      };
+
+    service3 = new google.maps.places.PlacesService(map);
+    service3.nearbySearch(request3, callback);
+
+   function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(place);
+    }
+  }
+}
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map : map,
+            position : placeLoc
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        
+        });
+       
+    }
+   
+    infowindow = new google.maps.InfoWindow();
+}
+function getEntertainment() { 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: newYork,
+        zoom: 14
+    });
+    var request = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['entertainment'], //also work with types, look at APi suported types
+        
+      };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    var request2 = {
+        location: newYork,
+        radius: '5500',
+        type: ['museum'], //also work with types, look at APi suported types
+        
+      };
+
+    service2 = new google.maps.places.PlacesService(map);
+    service2.nearbySearch(request2, callback);
+
+    var request3 = {
+        location: newYork,
+        radius: '5500',
+        type: ['movie_theater'],
+    };
+    service3 = new google.maps.places.PlacesService(map);
+    service3.nearbySearch(request3, callback);
+
+    var request4 = {
+        location: newYork,
+        radius: '5500',
+        type: ['casino'],
+    };
+    service4 = new google.maps.places.PlacesService(map);
+    service4.nearbySearch(request4, callback);
+    
+   function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(place);
+    }
+  }
+}
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map : map,
+            position : placeLoc
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        
+        });
+       
+    }
+   
+    infowindow = new google.maps.InfoWindow();
+}
+function getFun() { 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: newYork,
+        zoom: 14
+    });
+    var request = {
+        location: newYork,
+        radius: '5500',
+        keyword: ['fun'], //also work with types, look at APi suported types
+        
+    };
+    
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    var request2 = {
+        location: newYork,
+        radius: '5500',
+        type: ['amusement_park'],
+    };
+    service2 = new google.maps.places.PlacesService(map);
+    service2.nearbySearch(request2, callback);
+
+    var request3 = {
+        location: newYork,
+        radius: '5500',
+        type: ['aquarium'],
+    };
+    service3 = new google.maps.places.PlacesService(map);
+    service3.nearbySearch(request3, callback);
+    
+    var request4 = {
+        location: newYork,
+        radius: '5500',
+        type: ['zoo'],
+    };
+    service4 = new google.maps.places.PlacesService(map);
+    service4.nearbySearch(request4, callback);
+    
+
+   function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(place);
+    }
+  }
+}
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map : map,
+            position : placeLoc
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        
+        });
+       
+    }
+   
+    infowindow = new google.maps.InfoWindow();
+}
+      
+
+
+//The initial map
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 40.712776, lng: -74.005974},
       zoom: 13
     });
     var input = document.getElementById('searchInput');
-   
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
-
     var infowindow = new google.maps.InfoWindow();
     var marker = new google.maps.Marker({
         map: map,
@@ -28,18 +472,18 @@ function initMap() {
         infowindow.close();
         marker.setVisible(false);
         var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            window.alert("Autocomplete's returned place contains no geometry");
-            return;
-        }
-  
-        // If the place has a geometry, then present it on a map.
+
         if (place.geometry.viewport) {
             map.fitBounds(place.geometry.viewport);
         } else {
             map.setCenter(place.geometry.location);
             map.setZoom(17);
         }
+        if (!place.geometry) {
+            window.alert("No details available for input: ");
+            return;
+        }
+  
         marker.setIcon(({
             url: place.icon,
             size: new google.maps.Size(71, 71),
@@ -47,10 +491,11 @@ function initMap() {
             anchor: new google.maps.Point(17, 34),
             scaledSize: new google.maps.Size(35, 35)
         }));
+        var address = '';
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
     
-        var address = '';
+       
         if (place.address_components) {
             address = [
               (place.address_components[0] && place.address_components[0].short_name || ''),
@@ -62,21 +507,11 @@ function initMap() {
         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
         infowindow.open(map, marker);
       
-     
     });
 }
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
-}
-// function changeMap() {
-//     document.getElementById("map")
-// }
 
+
+//dropdown menu for media size
 document.addEventListener('click', e=> {
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
     if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
@@ -94,6 +529,7 @@ document.addEventListener('click', e=> {
 
 })
 
+//about-page stuff
 readMoreBtn.addEventListener('click', (e)=>{
     text.classList.toggle('show-more');
     if(readMoreBtn.innerText === 'Read More'){
